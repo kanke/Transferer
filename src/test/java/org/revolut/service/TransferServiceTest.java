680c1a365e94d2a9692320624127e2f6b3939435
Kanke <sugarkanke@yahoo.com>
2020-02-28 08:54:02 +0000

    Adding mockito dependency and failing transfer service test

---
+++ src/test/java/org/revolut/service/TransferServiceTest.java
@@ -0,0 +1,14 @@
+package org.revolut.service;
+
+import com.google.errorprone.annotations.DoNotMock;
+import org.mockito.Mock;
+
+public class TransferServiceTest {
+
+    @Mock
+    private TransferService transferService;
+
+    @Mock
+    private TransferRepository transferRepository;
+
+}
